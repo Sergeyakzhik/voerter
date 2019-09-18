2ac9ed1e67b5abd2b7dc2aa88a51e9fdb239a056
Sergeyakzhik <sergeyakzhik@gmail.com>
2019-09-19 02:58:18 +0300

    Updates

---
+++ precache-manifest.44e7197e07b84a9985f3082aed59558b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a3beeb008a1a452c14ff250a97b6f7",
+    "url": "/woerter/index.html"
+  },
+  {
+    "revision": "7a49c5d6135f2e59058a",
+    "url": "/woerter/static/css/main.eddf713d.chunk.css"
+  },
+  {
+    "revision": "73a765563d1d3f30bde7",
+    "url": "/woerter/static/js/2.ea3d7285.chunk.js"
+  },
+  {
+    "revision": "7a49c5d6135f2e59058a",
+    "url": "/woerter/static/js/main.5e80f435.chunk.js"
+  },
+  {
+    "revision": "4081675aee71da536208",
+    "url": "/woerter/static/js/runtime~main.5118c8e8.js"
+  }
+]);
